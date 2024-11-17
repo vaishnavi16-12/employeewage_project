01bfc88411992f0cbb6cbacd5f189afa246e910b
vaishnavi16-12 <vaishnavic16502@gmail.com>
2024-11-17 11:37:50 +0530

    Added:Add Part time Employee & Wage

--- src/main/java/com/bridgelabz/employeeproject/EmployeeWageMain.java
+++ src/main/java/com/bridgelabz/employeeproject/EmployeeWageMain.java
@@ -4,14 +4,20 @@ public class EmployeeWageMain {
    public static void main(String []arg){
        System.out.println("Welcome to Employee Wage Project");
 
-       final int FULL_TIME = 1;
+       final int FULL_TIME = 2;
+       int PART_TIME = 1;
        int WAGE_PER_HR = 20;
        int workingHours = 0;
-       double empType = Math.floor(Math.random() * 10) % 2;
+       double empType = Math.floor(Math.random() * 10) % 3;
        if (empType == FULL_TIME)
        {
-           System.out.println("Employee is Present");
+           System.out.println("Employee is Present Full Time");
            workingHours = 8;
+       }
+       else if (empType == PART_TIME)
+       {
+           System.out.println("Employee is Present Part Time");
+           workingHours = 4;
        }
        else
        {
@@ -21,6 +27,3 @@ public class EmployeeWageMain {
        System.out.println("Employee Daily Wage is " + wage);
    }
 }
-
-
-
