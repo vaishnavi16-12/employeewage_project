892bf04499a648b6af4f5604060c8059a5b28fb8
vaishnavi16-12 <vaishnavic16502@gmail.com>
2024-11-17 11:16:22 +0530

    Added : Welcome to Employee Wage Project

---
+++ src/main/java/com/bridgelabz/employeeproject/EmployeeWageMain.java
@@ -0,0 +1,10 @@
+package com.bridgelabz.employeeproject;
+
+public class EmployeeWageMain {
+   public static void main(String []arg){
+       System.out.println("Welcome to Employee Wage Project");
+
+   }
+
+
+}
