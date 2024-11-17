6a468a9760200e373781245e4c5b1e65fbe01002
vaishnavi16-12 <vaishnavic16502@gmail.com>
2024-11-17 11:24:39 +0530

    Added : Check Employee is Present or Absent

--- src/main/java/com/bridgelabz/employeeproject/EmployeeWageMain.java
+++ src/main/java/com/bridgelabz/employeeproject/EmployeeWageMain.java
@@ -4,6 +4,16 @@ public class EmployeeWageMain {
    public static void main(String []arg){
        System.out.println("Welcome to Employee Wage Project");
 
+       final int FULL_TIME = 1;
+       double empType = Math.floor(Math.random() * 10) % 2;
+       if (empType == FULL_TIME)
+       {
+           System.out.println("Employee is Present");
+       }
+       else
+       {
+           System.out.println("Employee is Absent");
+       }
    }
 
 
