c2cefe37092ef5a2d87e4188bb997cbb4ccc5584
vaishnavi16-12 <vaishnavic16502@gmail.com>
2024-11-17 15:42:46 +0530

    Added:solving using switch case

--- src/main/java/com/bridgelabz/employeeproject/EmployeeWageMain.java
+++ src/main/java/com/bridgelabz/employeeproject/EmployeeWageMain.java
@@ -5,23 +5,22 @@ public class EmployeeWageMain {
        System.out.println("Welcome to Employee Wage Project");
 
        final int FULL_TIME = 2;
-       int PART_TIME = 1;
+       final int PART_TIME = 1;
        int WAGE_PER_HR = 20;
        int workingHours = 0;
-       double empType = Math.floor(Math.random() * 10) % 3;
-       if (empType == FULL_TIME)
-       {
-           System.out.println("Employee is Present Full Time");
-           workingHours = 8;
-       }
-       else if (empType == PART_TIME)
-       {
-           System.out.println("Employee is Present Part Time");
-           workingHours = 4;
-       }
-       else
-       {
-           System.out.println("Employee is Absent");
+       int empType = (int) (Math.random() * 10) % 3;
+
+       switch (empType) {
+           case FULL_TIME:
+               System.out.println("Employee is Present Full Time");
+               workingHours = 8;
+               break;
+           case PART_TIME:
+               System.out.println("Employee is Present Part Time");
+               workingHours = 4;
+               break;
+           default:
+               System.out.println("Employee is Absent");
        }
        int wage = workingHours * WAGE_PER_HR;
        System.out.println("Employee Daily Wage is " + wage);
