b2665c323f135db3e4436ab2c4082c21ba1eba2f
vaishnavi16-12 <vaishnavic16502@gmail.com>
2024-11-17 15:49:28 +0530

    Added: Calculating wage in Month

--- src/main/java/com/bridgelabz/employeeproject/EmployeeWageMain.java
+++ src/main/java/com/bridgelabz/employeeproject/EmployeeWageMain.java
@@ -6,23 +6,26 @@ public class EmployeeWageMain {
 
        final int FULL_TIME = 2;
        final int PART_TIME = 1;
-       int WAGE_PER_HR = 20;
-       int workingHours = 0;
-       int empType = (int) (Math.random() * 10) % 3;
+       final int WAGE_PER_HR = 20;
+       final int WORKING_DAYS = 30;
+       int totalWage = 0;
+       for (int day = 1; day <= WORKING_DAYS; day++) {
+           int workingHours = 0;
+           int empType = (int) (Math.random() * 10) % 3;
 
-       switch (empType) {
-           case FULL_TIME:
-               System.out.println("Employee is Present Full Time");
-               workingHours = 8;
-               break;
-           case PART_TIME:
-               System.out.println("Employee is Present Part Time");
-               workingHours = 4;
-               break;
-           default:
-               System.out.println("Employee is Absent");
+           switch (empType) {
+               case FULL_TIME:
+                   workingHours = 8;
+                   break;
+               case PART_TIME:
+                   workingHours = 4;
+                   break;
+               default:
+           }
+           int wage = workingHours * WAGE_PER_HR;
+           System.out.println("Day " + day + " wage is:" + wage);
+           totalWage += wage;
        }
-       int wage = workingHours * WAGE_PER_HR;
-       System.out.println("Employee Daily Wage is " + wage);
+       System.out.println("Total wage for a month is " + totalWage);
    }
 }
